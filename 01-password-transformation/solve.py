9f6e8d296a3e981239b2de6c3b0a2993ac5e0215
JuanMendozaSec <109883440+JuanMendozaSec@users.noreply.github.com>
2026-09-02 11:29:12 -0500

    Implement password transformation logic

--- 01-password-transformation/solve.py
+++ 01-password-transformation/solve.py
@@ -1,1 +1,13 @@
 
+expected = [
+    0x49, 0x66, 0x64, 0x75,
+    0x69, 0x48, 0x71, 0x70
+]
+
+password = ""
+
+for byte in expected:
+    original = (byte ^ 0x20) - 3
+    password += chr(original)
+
+print(password)
